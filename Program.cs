7bee9a3175278eccbb239ae7a0d10a7e659b1df6
eduar <eduar@DESKTOP-RF8VGOV>
2023-02-25 12:04:09 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a cotação do dólar?");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+    }
+}
