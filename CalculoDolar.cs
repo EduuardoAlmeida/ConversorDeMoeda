1d24a374b4538aae02e1a67aa1f36ffa0e22563c
Eduardo Almeida <106634774+EduuardoAlmeida@users.noreply.github.com>
2023-02-25 15:19:22 -0300

    Criado o cálculo do dólar

---
+++ CalculoDolar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoeda
+{
+    public class CalculoDolar
+    {
+        public static double CalculoDolar(double cotacao, double quantidade)
+        {
+            double valorDolar = cotacao * quantidade;
+            double iof = 0.06;
+
+            return (valorDolar * iof) + valorDolar;
+        }
+    }
+}
